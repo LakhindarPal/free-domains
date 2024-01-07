baf2d9c1bf1515b82dd1b9a55aa89a3aa528c9f8
Lakhindar Pal <72681474+LakhindarPal@users.noreply.github.com>
2024-01-07 17:01:30 +0530

    Create myanijot.is-an.app.js

---
+++ domains/myanijot.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'A website to keep track of the anime I've watched',
+  domain: 'is-an.app',
+  subdomain: 'myanijot',
+  owner: {
+    email: 'dev.lakhindarpal@gmail.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: true
+});
